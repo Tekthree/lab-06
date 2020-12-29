8b514afa7aeb501d4a17c7d77798ae9dce4d7646
tekthree <tekthree@gmail.com>
2020-12-28 22:37:13 -0800

    finished listeners

--- server.js
+++ server.js
@@ -11,3 +11,43 @@ app.use(cors());
 app.use('*', (request,response)=> {
   response.send('404 does not exsist')
 });
+
+// Routes
+
+app.get('/', homeHandlr);
+app.get('/location', locationHandler);
+app.get('/weather', wtrHandler);
+app.use('*', errorHandler);
+
+function homeHandlr(request, response){
+  response.send('Sup Yo');
+}
+
+function errorHandler(request, response){
+  response.status(500).send('You Busted, Everythings gone wrong');
+}
+
+function locationHandler(request, response){
+  const locationData = require('./data/location.json');
+}
+
+
+//Constructor
+function Location(city, LocationData){
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+function Weather(data){
+  this.forecast = data.weather.description;
+  let date = Date.parse(data.datetime);
+  this.time = new Date(date).toDateString();
+}
+
+//start server
+
+app.listen(PORT, ()=>{
+  console.log(`Listening on ${PORT}`);
+}
