fe25f7df6f6218eb8c55cc3174801e3265a02522
tekthree <tekthree@gmail.com>
2021-01-13 20:58:36 -0800

    fixed superagent

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const superagent = require('superagent');
+// const superagent = require('superagent');
 
 //start app
 // specify your port
